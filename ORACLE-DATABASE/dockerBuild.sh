879f7b18de23bdf447f21162a7f44b80fbfd5d9e
Lorenzo Lince <lorenzo.lince@smartmatic.com>
2019-11-12 12:07:05 -0500

    create oracle database and kafka

---
+++ ORACLE-DATABASE/dockerBuild.sh
@@ -0,0 +1,4 @@
+#Author Lorenzo Lince
+#Shell Script automates the oracle database docker build
+#!/bin/bash
+docker build --tag=oracle-db .
---
+++ ORACLE-DATABASE/init/users.sql
@@ -0,0 +1,5 @@
+CREATE USER master identified by master;
+GRANT CONNECT TO master;
+GRANT ALL PRIVILEGES TO master;
+
+
